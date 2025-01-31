408277c4f19e421dbf3fd4176b4688b3cef97dde
Calvin <munenecalvn@gmail.com>
2025-01-31 09:57:28 +0300

    Add token types and list for authentication management

---
+++ src/config/tokens.js
@@ -0,0 +1,13 @@
+const tokenTypes = {
+  ACCESS: 'access',
+  REFRESH: 'refresh',
+  RESET_PASSWORD: 'resetPassword',
+  VERIFY_EMAIL: 'verifyEmail',
+};
+
+const tokenTypeList = Object.values(tokenTypes);
+
+module.exports = {
+  tokenTypes,
+  tokenTypeList
+};
