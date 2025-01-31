2904c08db5a5b0237a5aaaece51d2c36984c36b9
Calvin <munenecalvn@gmail.com>
2025-01-31 09:54:15 +0300

    Add security headers, CORS support, and JSON body parsing to Express app

--- src/index.js
+++ src/index.js
@@ -29,3 +29,60 @@ mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
         logger.info(`Listening to port ${config.port}`);
     });
 });
+
+// set security HTTP headers
+app.use(helmet());
+
+// enable cors
+app.use(cors());
+app.options('*', cors());
+app.use(timeout('180s'));
+
+// parse json request body
+app.use(express.json({ limit: '50mb' }));
+
+// parse urlencoded request body
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
+
+app.disable('x-powered-by');
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization');
+    res.header('X-Frame-Options', 'DENY'); // if its required to change, update it to SAMEORIGIN and retest
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('X-Content-Type-Options', 'nosniff');
+    res.header('X-XSS-Protection', '1; mode=block');
+    res.header('Strict-Transport-Security', 'max-age=15552000; includeSubDomains');
+    res.header('X-DNS-Prefetch-Control', 'Off');
+    res.header('X-Download-Options', 'noopen');
+    res.header(
+        'Content-Security-Policy',
+        "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'",
+    );
+    return next();
+});
+
+// jwt authentication
+app.use(passport.initialize());
+passport.use('jwt', jwtStrategy);
+
+
+// Api routes
+app.get('/', (req: Request , res: Response) => {
+    res.send("Hello, welcome to the Backend Template...... 🚀👋");
+});
+
+app.use('/v1', routes);
+
+// send back a 404 error for any unknown api request
+app.use((req: Request, res: Response, next: NextFunction) => {
+    return next(new ApiError(404, 'Not Found'));
+});
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
+
+export default app;
