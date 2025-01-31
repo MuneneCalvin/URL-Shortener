0454ca65b91411ac5503e12b6cbd9c66c101efff
Calvin <munenecalvn@gmail.com>
2025-01-31 09:34:04 +0300

    Initialize server setup with Express, Helmet, Passport, CORS, and Mongoose

---
+++ src/index.js
@@ -0,0 +1,7 @@
+require('dotenv').config();
+
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const cors = require('cors');
+const mongoose = require('mongoose');
