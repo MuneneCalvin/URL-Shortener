577db4e5b090ff0e70d248560bceaca0049f7b39
Calvin <munenecalvn@gmail.com>
2025-01-31 09:50:26 +0300

    Initialize Express app and set up middleware for logging, error handling, and routing

--- src/index.js
+++ src/index.js
@@ -5,3 +5,14 @@ const helmet = require('helmet');
 const passport = require('passport');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const config = require('./config/config');
+const logger = require('./config/logger');
+const morgan = require('./config/morgan');
+import { jwtStrategy } from './config/passport';
+import ApiError from './utils/ApiError';
+import { errorConverter, errorHandler } from './middlewares/error';
+import routes from './routes';
+
+
+const app = express();
+let server;
