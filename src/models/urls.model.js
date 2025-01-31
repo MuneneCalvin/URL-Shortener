2308329a0ebaa0801b8eaad8ca0523c58ac5cc58
Calvin <munenecalvn@gmail.com>
2025-01-31 11:51:50 +0300

    Add URL model with schema for original and short URLs

---
+++ src/models/urls.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const shortid = require('shortid');
+
+const urlSchema = new mongoose.Schema({
+    originalUrl: {
+        type: String,
+        required: true,
+    },
+    shortUrl: {
+        type: String,
+        required: true,
+        default: shortid.generate
+    },
+    clicks: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Url', urlSchema);
