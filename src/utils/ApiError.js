77bac6abf94bdd24d657d10cab3ee24d3a2c08b8
Calvin <munenecalvn@gmail.com>
2025-02-03 14:55:23 +0300

    Change ApiError export to CommonJS module format

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -1,11 +1,8 @@
 class ApiError extends Error {
-
   constructor(statusCode, message, isOperational = true, stack = '') {
     super(message);
-
     this.statusCode = statusCode;
     this.isOperational = isOperational;
-
     if (stack) {
       this.stack = stack;
     } else {
@@ -14,4 +11,4 @@ class ApiError extends Error {
   }
 }
 
-export default ApiError;
+module.exports = ApiError;
