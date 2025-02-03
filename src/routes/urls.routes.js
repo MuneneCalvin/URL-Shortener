5ae628da66baaad1548c5217c63960b195ac8e12
Calvin <munenecalvn@gmail.com>
2025-02-03 14:43:43 +0300

    Remove token verification from shorten, update, and delete URL routes

--- src/routes/urls.routes.js
+++ src/routes/urls.routes.js
@@ -4,11 +4,11 @@ const urlsController = require('../controllers/urls.controller');
 
 const router = express.Router();
 
-router.post('/shorten', verifyToken, urlsController.createShortUrl);
+router.post('/shorten', urlsController.createShortUrl);
 router.get('/:shortUrl', urlsController.getOriginalUrl);
 router.get('/', urlsController.getAllUrls);
-router.put('/', verifyToken, urlsController.updateShortUrl);
-router.delete('/:shortUrl', verifyToken, urlsController.deleteUrl);
+router.put('/', urlsController.updateShortUrl);
+router.delete('/:shortUrl', urlsController.deleteUrl);
 
 
 module.exports = router;
