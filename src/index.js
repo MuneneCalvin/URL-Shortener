85d8da0e574470801076534c5faa985cba3b8085
Calvin <munenecalvn@gmail.com>
2025-02-04 10:55:47 +0300

    Add body-parser middleware for JSON request handling

--- src/index.js
+++ src/index.js
@@ -58,6 +58,7 @@ app.use(express.json({ limit: '50mb' }));
 
 // parse urlencoded request body
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
+app.use(bodyParser.json());
 
 app.disable('x-powered-by');
 app.use((req, res, next) => {
